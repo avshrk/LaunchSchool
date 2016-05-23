c76cc7672c822301f8cb84a14f25c30c80ca62b3
avshrk <avshark@gmail.com>
2016-05-23 13:31:54 -0400

    Run Rubocop

--- back-end-dev/lesson_4/exercises/twentyone.rb
+++ back-end-dev/lesson_4/exercises/twentyone.rb
@@ -2,7 +2,8 @@ require 'byebug'
 require 'pry'
 
 DECK =  { 'Suite' => ['H', 'D', 'C', 'S'],
-          'Value' => ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'] }.freeze
+          'Value' => ['2', '3', '4', '5', '6', '7', '8'] +
+                     ['9', '10', 'J', 'Q', 'K', 'A'] }.freeze
 SUITE = 0
 VALUE = 1
 ACE = 'A'.freeze
@@ -33,7 +34,7 @@ end
 def deal_uniq_card(pre_draws)
   new_draw = draw_card
   loop do
-    break unless already_drawn?(pre_draws,new_draw)
+    break unless already_drawn?(pre_draws, new_draw)
     new_draw = draw_card
   end
   new_draw
@@ -52,14 +53,14 @@ def ace_card?(card)
 end
 
 def get_card_value(card)
- return ACE_ONE if ace_card?(card)
- return FACE_CARD if face_card?(card)
- return card[VALUE].to_i
+  return ACE_ONE if ace_card?(card)
+  return FACE_CARD if face_card?(card)
+  card[VALUE].to_i
 end
 
-def get_highest_sum(cards,cards_sum)
+def get_highest_sum(cards, cards_sum)
   return cards_sum unless ace_exist?(cards)
-  return ( cards_sum + TEN ) <= TWENTY_ONE ? (cards_sum + TEN) : cards_sum
+  (cards_sum + TEN) <= TWENTY_ONE ? (cards_sum + TEN) : cards_sum
 end
 
 def calculate_hand(cards)
@@ -67,7 +68,7 @@ def calculate_hand(cards)
   cards.each do |each_card|
     total_card_value += get_card_value(each_card)
   end
-   get_highest_sum(cards, total_card_value)
+  get_highest_sum(cards, total_card_value)
 end
 
 def display_cards(cards)
@@ -75,10 +76,10 @@ def display_cards(cards)
   cards.each do |each_card|
     hand += each_card[1].to_s + ' '
   end
-  prompt hand
+  hand
 end
 
-def deal_hand(cards,dealt_cards)
+def deal_hand(cards, dealt_cards)
   2.times { cards << deal_uniq_card(dealt_cards) }
 end
 
@@ -92,7 +93,7 @@ def joinor(list, delimiter = ',', conjunction = 'or')
   join_list.join(delimiter)
 end
 
-def inquire_player(message,valid_choices)
+def inquire_player(message, valid_choices)
   prompt message
   answer = ''
   loop do
@@ -101,12 +102,13 @@ def inquire_player(message,valid_choices)
     prompt "It is not a valid choice."
     prompt "Please type : #{joinor(valid_choices)}"
   end
-  return answer
+  answer
 end
 
 def stay?
- answer =  inquire_player("Would you like to stay or hit ? ( stay/hit )", ['stay','hit'])
- answer == 'stay'
+  message = "Would you like to stay or hit ? ( stay/hit )"
+  answer =  inquire_player(message, ['stay', 'hit'])
+  answer == 'stay'
 end
 
 def busted?(hand)
@@ -122,31 +124,32 @@ def dealer_plays(hand, pre_cards)
   loop do
     break if busted?(hand) || dealer_stops?(hand)
     hand << deal_uniq_card(pre_cards)
-    display_cards(hand)
+    prompt  display_cards(hand)
     sleep(1)
   end
 end
 
 def play_again?
- answer = inquire_player("Would you like to play again ? (yes,no)", ['yes','no'])
- answer == 'yes'
+  message = "Would you like to play again ? (yes,no)"
+  answer = inquire_player(message, ['yes', 'no'])
+  answer == 'yes'
 end
 
 def hide_dealer_first_card(cards)
-  [ ['Hidden', cards[0][1] ]]
+  [['Hidden', 'Hidden'], cards[1]]
 end
 
 def ace_exist?(cards)
   cards.flatten.include?(ACE)
 end
 
-def player_won?(player_cards,dealer_cards)
+def player_won?(player_cards, dealer_cards)
   return nil if busted?(player_cards)
   return true if busted?(dealer_cards)
   calculate_hand(player_cards) > calculate_hand(dealer_cards)
 end
 
-def tie?(player_cards,dealer_cards)
+def tie?(player_cards, dealer_cards)
   calculate_hand(player_cards) == calculate_hand(dealer_cards)
 end
 
@@ -154,63 +157,62 @@ def dealers_turn(dealer_hand, out_hands)
   loop do
     dealer_plays(dealer_hand, out_hands)
     break if busted?(dealer_hand) || dealer_stops?(dealer_hand)
-    display_cards(dealer_hand)
   end
+  display_cards(dealer_hand)
 end
 
 def players_turn(player_hand, out_hands)
   loop do
     break if busted?(player_hand) || stay?
     player_hand << deal_uniq_card(out_hands)
-    display_cards(player_hand)
+    prompt display_cards(player_hand)
   end
 end
 
-def display_winner(player_hand,dealer_hand)
-  return  "It is a tie!" if tie?(player_hand, dealer_hand)
+def display_winner(player_hand, dealer_hand)
+  return "It is a tie!" if tie?(player_hand, dealer_hand)
 
-  if player_won?(player_hand,dealer_hand)
-    return  "You Won!!"
+  if player_won?(player_hand, dealer_hand)
+    return "You Won!!"
   else
-    return  "Dealer Won!! "
+    return "Dealer Won!! "
   end
+end
+
+def deal_hands(player_hand, dealer_hand)
+  deal_hand(player_hand, player_hand + dealer_hand)
+  deal_hand(dealer_hand, player_hand + dealer_hand)
+end
+
+def deal_cards(player_hand, dealer_hand)
+  deal_hands(player_hand, dealer_hand)
+  prompt "Your Cards: #{display_cards(player_hand)}"
+  prompt "Dealer Card: #{display_cards(hide_dealer_first_card(dealer_hand))} "
 end
 
 def twenty_one
   loop do
-
     system_clear
     player_hand = []
     dealer_hand = []
 
     prompt "Lets Play Black Jack"
-    deal_hand(player_hand, player_hand + dealer_hand)
-    deal_hand(dealer_hand, player_hand + dealer_hand)
-    prompt "Your Cards: "
-    display_cards(player_hand)
-    prompt "Dealer Card: "
-    display_cards(hide_dealer_first_card(dealer_hand))
-    prompt "Your turn to play."
+    deal_cards(player_hand, dealer_hand)
     players_turn(player_hand, player_hand + dealer_hand)
+    prompt "Dealers Hand : #{display_cards(dealer_hand)} "
 
     if busted?(player_hand)
-      prompt "Busted!!"
-      prompt "Your Total : #{calculate_hand(player_hand)}"
-      prompt "Dealer Cards:"
-      display_cards(dealer_hand)
+      prompt "Busted!! Your Total : #{calculate_hand(player_hand)}"
     else
-      prompt "You chose to stay"
-      prompt "Your Total : #{calculate_hand(player_hand)}"
-      prompt "Dealer Cards:"
-      display_cards(dealer_hand)
-      dealers_turn(dealer_hand, player_hand + dealer_hand)
+      prompt "You chose to stay. Your Total : #{calculate_hand(player_hand)}"
+      out_cards = player_hand + dealer_hand
+      prompt "Dealer Hand: #{dealers_turn(dealer_hand, out_cards)}"
     end
 
     prompt "Dealer's Total : #{calculate_hand(dealer_hand)}"
-    prompt display_winner(player_hand,dealer_hand)
+    prompt display_winner(player_hand, dealer_hand)
     break unless play_again?
   end
 end
 
 twenty_one
-
