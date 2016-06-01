9fe0a9350c8cddb3a56b6379e6b4c5d5ef30b61e
avshrk <avshark@gmail.com>
2016-05-31 20:44:43 -0400

    Refactor Car Loan Calculator

--- back_end_dev/lesson_2/car_loan_calculator.rb
+++ back_end_dev/lesson_2/car_loan_calculator.rb
@@ -1,12 +1,13 @@
-def cal_monthly_rate(annual_rate)
+
+def monthly_rate(annual_rate)
   annual_rate / 1200
 end
 
-def cal_loan_duration_months(loan_annual)
+def loan_duration_months(loan_annual)
   loan_annual * 12
 end
 
-def cal_payments(rate, duration, amount)
+def payments(rate, duration, amount)
   amount * (rate * (1 + rate)**duration) / (((1 + rate)**duration) - 1)
 end
 
@@ -14,10 +15,8 @@ def prompt(str)
   print "==> " + str
 end
 
-prompt("Car Loan Calculator\n")
-
-loop do
-  loan_amount = 0
+def given_loan_amount
+  loan_amount = ''
   prompt('Enter loan amount (without $ sign or comma) : ')
 
   loop do
@@ -26,20 +25,25 @@ loop do
     break if loan_amount > 0
     prompt('Invalid amount, Enter a value larger than 0 : ')
   end
+  loan_amount
+end
 
+def given_annual_rate
   annual_rate = 0
   prompt('Enter annual interest rate (6% as 6 without percent sign) : ')
 
   loop do
     annual_rate = gets.chomp.to_f
 
-    break if annual_rate >= 0
+    break if annual_rate > 0
     prompt("Invalid Rate, Enter a value larger than 0 : ")
   end
+  annual_rate
+end
 
-  monthly_rate = cal_monthly_rate(annual_rate)
-
+def given_loan_duration
   loan_duration = 0
+
   prompt('Enter loan duration in years: ')
   loop do
     loan_duration = gets.chomp.to_i
@@ -47,13 +51,28 @@ loop do
     break if loan_duration > 0
     prompt("Invalid Duration, Enter a value larger than 0 : ")
   end
+  loan_duration
+end
 
-  loan_months = cal_loan_duration_months(loan_duration)
-
-  prompt("Your monthly payment: " + cal_payments(monthly_rate, loan_months, loan_amount).round(2).to_s + "\n")
-
+def continue?
   prompt('Enter y to continue, any key to quit: ')
   answer = gets.chomp[0].downcase
-
-  break if answer != 'y'
+  answer.start_with?('y')
 end
+
+def car_loan_calculator
+  prompt("Car Loan Calculator\n")
+
+  loop do
+    loan_amount = given_loan_amount
+    annual_rate = given_annual_rate
+    monthly_rate = monthly_rate(annual_rate)
+    loan_duration = given_loan_duration
+    loan_months = loan_duration_months(loan_duration)
+
+    prompt("Your monthly payment: " + payments(monthly_rate, loan_months, loan_amount).round(2).to_s + "\n")
+
+    break unless continue?
+  end
+end
+car_loan_calculator
