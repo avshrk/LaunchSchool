f17e99434a2d7f49dd35877faddd0914a5d66eb9
avshrk <avshark@gmail.com>
2017-08-18 14:58:05 -0400

    Update Notes

--- front_end_dev/220/220_notes/upto_Lesson6/objects.js
+++ front_end_dev/220/220_notes/upto_Lesson6/objects.js
@@ -1,192 +1,88 @@
-
-var me = {
-  first: 'joe',
-  last: 'doe'
+var me =  {
+  first_name: 'John',
+  last_name: 'doe'
 };
 
-var friend = {
-  first: 'bar',
-  last: 'boe'
+var friend =  {
+  first_name: 'Micheal',
+  last_name: 'Smith'
 };
 
-var friend2 = {
-  first: 'bar2',
-  last: 'boe2'
+var mother =  {
+  first_name: 'sue',
+  last_name: 'Smith'
 };
 
-var friend1 = {
-  first: 'bar1',
-  last: 'boe1'
+var father =  {
+  first_name: 'dave',
+  last_name: 'Smith'
 };
+
 
 var people = {
-  list: [],
-  fullName: function (person){
-    console.log(person.first + ' ' + person.last);
+  collection: [me, mother, father],
+  fullName: function(person){
+    console.log(person.first_name + '  ' + person.last_name);
   },
   rollCall: function(){
-    this.list.forEach(this.fullName);
-  },
-  add: function(person){
-    this.list.push(person);
-  },
-  isInvalidPerson: function (person){
-    return typeof person.first !== 'string' || typeof person.last !== 'string';
-  },
-  get: function(person){
-
-    var index = this.getIndex(person);
-    if (index < 0) return;
-
-    return this.list[index];
-
-  },
-  update: function(person){
-
-    if (this.isInvalidPerson(person)) return;
-
-    var index = this.getIndex(person);
-
-    if (index < 0) {
-      this.add(person);
-    } else {
-      this.list[index] = person;
-    }
-  },
-  getIndex: function(person){
-    var index = -1;
-
-    this.list.forEach(function(item,i){
-      if (person.last === item.last && person.first === item.first ){
-        index = i ;
-      }
-    });
-    return index;
-  },
-  remove: function(person){
-
-    if (this.isInvalidPerson(person)) return;
-
-    var index = this.getIndex(person);
-    if (index < 0) return;
-
-    this.list.splice(index, 1);
-  },
-};
-
-function pr(text){
-  console.log(text);
-}
-zi = {};
-people.add(me);
-people.add(friend);
-people.add(friend2);
-people.add(friend1);
-people.rollCall();
-people.remove(zi);
-pr('removed friend2');
-people.rollCall();
-pr(people.isInvalidPerson(me));
-pr(people.isInvalidPerson(zi));
-pr(people.get(me));
-
-/*
-var me = {
-  first: 'John',
-  last:'Doe',
-};
-
-var friend = {
-  first: 'Jane',
-  last: 'Doe',
-};
-
-var people = {
-  collection: [],
-  add: function(person){
-    this.collection.push(person);
-  },
-  fullName: function(person){
-   console.log(person.first + '  ' + person.last);
-  },
-  rollCall: function (){
     this.collection.forEach(this.fullName);
   },
-  getIndex: function(person,i){
-    var index = -1;
-    this.collection.forEach(function(eachPerson, i){
-      if (person.fullName === eachPerson.fullName){
-        index = i;
-      }
-    });
-   return index;
+  add: function(person){
+    if (this.isInvalidPerson(person)) return;
+
+    this.collection.push(person);
   },
+  getIndex: function(person){
+    return this.collection.findIndex(function(element,index){
+      return element.first_name === person.first_name &&
+        element.last_name === person.last_name;
+    });
+  },
+  remove: function(person){
+    if (this.isInvalidPerson(person)) return;
+
+    var index = this.getIndex(person);
+    if ( index === -1 ) return ;
+
+    this.collection.splice(index,1);
+  },
+  isInvalidPerson: function(person){
+    return (typeof person.first_name !== 'string' ||
+      typeof person.last_name !== 'string');
+  },
+  get: function(person){
+    if (this.isInvalidPerson(person)) return;
+
+    return this.collection[this.getIndex(person)];
+  },
+  update: function(person){
+    var index = this.getIndex(person);
+
+    if (index === -1 ){
+      this.add(person);
+    } else {
+      this.collection[index] = person;
+    }
+  }
 };
-
-people.add(me);
-people.add(friend);
-people.rollCall();
-var index = people.getIndex(me);
-console.log(index);
-
-
-var me = {
-  first: 'John',
-  last:'Doe',
-};
-
-var friend = {
-  first: 'Jane',
-  last: 'Doe',
-};
-
-var people = [];
-
-people.push(me);
-people.push(friend);
 
 function fullName(person) {
-  console.log(person.first + '  ' + person.last);
+  console.log(person.first_name + ' ' + person.last_name);
 }
 
-function rollCall(people) {
+function rollCall(people){
   people.forEach(fullName);
 }
 
-rollCall(people);
+people.rollCall();
+people.add(friend);
+console.log('   ');
+people.rollCall();
+console.log(people.getIndex(friend));
+people.remove(friend);
+console.log(people.getIndex(friend));
+people.rollCall();
+console.log(people.isInvalidPerson(friend));
 
+console.log(people.get(father));
 
-function makeCar(speedRate, breakRate) {
-  return {
-    speed: 0,
-    breakRate: breakRate,
-    speedRate: speedRate,
-    accelerate: function(){
-      return this.speed += this.speedRate;
-    },
-    break: function(){
-      this.speed -= this.breakRate;
-      if (this.speed < 0 ){
-        this.speed = 0;
-      }
-    }
-  };
-}
-
-var hatcback = makeCar(9,6);
-hatcback.accelerate();
-hatcback.accelerate();
-hatcback.accelerate();
-console.log(hatcback.speed);
-hatcback.break();
-console.log(hatcback.speed);
-hatcback.break();
-console.log(hatcback.speed);
-hatcback.break();
-hatcback.break();
-hatcback.break();
-
-console.log(hatcback.speed);
-
-
-*/
