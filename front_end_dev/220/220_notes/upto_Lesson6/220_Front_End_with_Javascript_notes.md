b2f1fc4a33bdef4b610c65f452bdbf2599c35ce4
avshrk <avshark@gmail.com>
2017-08-18 16:09:40 -0400

    Update Notes

--- front_end_dev/220/220_notes/upto_Lesson6/220_Front_End_with_Javascript_notes.md
+++ front_end_dev/220/220_notes/upto_Lesson6/220_Front_End_with_Javascript_notes.md
@@ -36,7 +36,7 @@
    }; // adding after object creation
 
     ```
-#### Object Methods
+### Object Methods
 ```javascript
     var people = {
       collection: [me, mother, father],
@@ -86,3 +86,41 @@
     };
 
 ```
+
+### Practice Problems
+```javascript
+
+var invoices = {
+  unpaid: [],
+  paid: [],
+  add: function(name, amount){
+    this.unpaid.push({
+      name: name,
+      amount: amount
+    });
+  },
+  totalDue: function(){
+    return this.getTotal(this.unpaid);
+  },
+  totalPaid: function(){
+    return this.getTotal(this.paid);
+  },
+  getTotal: function(collection){
+    return collection.reduce(function(total,invoice){
+      return total + invoice.amount;
+    },0);
+  },
+  payInvoice: function(name){
+    var temp_unpaid = [];
+    this.unpaid.forEach(function(invoice){
+      if (invoice.name === name){
+        this.paid.push(invoice);
+      } else {
+        temp_unpaid.push(invoice);
+      }
+    },this);
+    this.unpaid = temp_unpaid;
+  },
+};
+```
+
--- front_end_dev/220/220_notes/upto_Lesson6/objects.js
+++ front_end_dev/220/220_notes/upto_Lesson6/objects.js
@@ -1,88 +1,33 @@
-var me =  {
-  first_name: 'John',
-  last_name: 'doe'
-};
-
-var friend =  {
-  first_name: 'Micheal',
-  last_name: 'Smith'
-};
-
-var mother =  {
-  first_name: 'sue',
-  last_name: 'Smith'
-};
-
-var father =  {
-  first_name: 'dave',
-  last_name: 'Smith'
-};
-
-
-var people = {
-  collection: [me, mother, father],
-  fullName: function(person){
-    console.log(person.first_name + '  ' + person.last_name);
-  },
-  rollCall: function(){
-    this.collection.forEach(this.fullName);
-  },
-  add: function(person){
-    if (this.isInvalidPerson(person)) return;
-
-    this.collection.push(person);
-  },
-  getIndex: function(person){
-    return this.collection.findIndex(function(element,index){
-      return element.first_name === person.first_name &&
-        element.last_name === person.last_name;
+var invoices = {
+  unpaid: [],
+  paid: [],
+  add: function(name, amount){
+    this.unpaid.push({
+      name: name,
+      amount: amount
     });
   },
-  remove: function(person){
-    if (this.isInvalidPerson(person)) return;
-
-    var index = this.getIndex(person);
-    if ( index === -1 ) return ;
-
-    this.collection.splice(index,1);
+  totalDue: function(){
+    return this.getTotal(this.unpaid);
   },
-  isInvalidPerson: function(person){
-    return (typeof person.first_name !== 'string' ||
-      typeof person.last_name !== 'string');
+  totalPaid: function(){
+    return this.getTotal(this.paid);
   },
-  get: function(person){
-    if (this.isInvalidPerson(person)) return;
-
-    return this.collection[this.getIndex(person)];
+  getTotal: function(collection){
+    return collection.reduce(function(total,invoice){
+      return total + invoice.amount;
+    },0);
   },
-  update: function(person){
-    var index = this.getIndex(person);
-
-    if (index === -1 ){
-      this.add(person);
-    } else {
-      this.collection[index] = person;
-    }
-  }
+  payInvoice: function(name){
+    var temp_unpaid = [];
+    this.unpaid.forEach(function(invoice){
+      if (invoice.name === name){
+        this.paid.push(invoice);
+      } else {
+        temp_unpaid.push(invoice);
+      }
+    },this);
+    this.unpaid = temp_unpaid;
+  },
 };
-
-function fullName(person) {
-  console.log(person.first_name + ' ' + person.last_name);
-}
-
-function rollCall(people){
-  people.forEach(fullName);
-}
-
-people.rollCall();
-people.add(friend);
-console.log('   ');
-people.rollCall();
-console.log(people.getIndex(friend));
-people.remove(friend);
-console.log(people.getIndex(friend));
-people.rollCall();
-console.log(people.isInvalidPerson(friend));
-
-console.log(people.get(father));
 
